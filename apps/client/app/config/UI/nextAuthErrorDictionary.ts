ddfd924280468772f3d13c53a45fdaf30c1025a5
un-know-n <fa1thlezss@gmail.com>
2023-12-19 16:53:24 +0200

    FEAT(client): Update authentication errors description

--- apps/client/app/config/UI/nextAuthErrorDictionary.ts
+++ apps/client/app/config/UI/nextAuthErrorDictionary.ts
@@ -1,56 +1,28 @@
 export const NextAuthErrorDictionary = {
   Configuration:
-    'Помилка конфігурації: Виникла проблема із конфігурацією сервера. Будь ласка, перевірте налаштування, щоб впевнитися, що все в порядку.',
+    'Виникла проблема із конфігурацією сервера. Будь ласка, перевірте налаштування, щоб впевнитися, що все в порядку',
   AccessDenied:
-    "Відмовлено в доступі: Ви, можливо, були обмежені в доступі до цієї функції. Якщо ви вважаєте, що це помилка, будь ласка, зв'яжіться з підтримкою.",
+    "Ви були обмежені в доступі до цієї функції. Якщо ви вважаєте, що це помилка, будь ласка, зв'яжіться з підтримкою",
   Verification:
-    "Помилка верифікації: Токен може бути або простроченим, або вже використаним. Будь ласка, запросіть новий токен або зв'яжіться з підтримкою для отримання допомоги.",
+    "Токен може бути або простроченим, або вже використаним. Будь ласка, запросіть новий токен або зв'яжіться з підтримкою для отримання допомоги",
   OAuthSignin:
-    'Помилка авторизації: Виникла проблема у підготовці авторизації. Будь ласка, спробуйте ще раз, і якщо проблема продовжується, повідомте нас.',
+    'Виникла проблема у підготовці авторизації. Будь ласка, спробуйте ще раз, і якщо проблема продовжується, повідомте нас',
   OAuthCallback:
-    "Помилка обробки: Виникла проблема під час обробки відповіді від постачальника OAuth. Будь ласка, спробуйте ще раз, і якщо проблема триває, зв'яжіться з нами.",
+    "Виникла проблема під час обробки відповіді від постачальника OAuth. Будь ласка, спробуйте ще раз, і якщо проблема триває, зв'яжіться з нами",
   OAuthCreateAccount:
-    "Помилка створення облікового запису: Ми не змогли створити ваш обліковий запис із постачальником OAuth. Будь ласка, спробуйте інший спосіб реєстрації або зв'яжіться із підтримкою.",
+    "Ми не змогли створити ваш обліковий запис із постачальником OAuth. Будь ласка, спробуйте інший спосіб реєстрації або зв'яжіться із підтримкою",
   EmailCreateAccount:
-    "Помилка створення облікового запису: Ми не змогли створити ваш обліковий запис. Будь ласка, перевірте свою адресу електронної пошти та спробуйте ще раз, або зв'яжіться із підтримкою для отримання допомоги.",
+    "Ми не змогли створити ваш обліковий запис. Будь ласка, перевірте свою адресу електронної пошти та спробуйте ще раз, або зв'яжіться із підтримкою для отримання допомоги",
   Callback:
-    'Помилка зворотнього виклику: Щось пішло не так у нашій обробці зворотнього виклику. Будь ласка, спробуйте ще раз, і якщо проблема продовжується, повідомте нас.',
+    'Щось пішло не так у нашій обробці зворотнього виклику. Будь ласка, спробуйте ще раз, і якщо проблема продовжується, повідомте нас',
   OAuthAccountNotLinked:
-    "Обліковий запис не пов'язано: Ваша адреса електронної пошти вже пов'язана із іншим обліковим записом, але не із цим. Будь ласка, увійдіть із початковим обліковим записом.",
+    "Ваша адреса електронної пошти вже пов'язана із іншим обліковим записом, але не із цим. Будь ласка, увійдіть із початковим обліковим записом",
   EmailSignin:
-    "Помилка надсилання електронного листа із верифікаційним токеном. Будь ласка, перевірте адресу електронної пошти та спробуйте ще раз, або зв'яжіться з підтримкою для отримання допомоги.",
+    "Помилка надсилання електронного листа із верифікаційним токеном. Будь ласка, перевірте адресу електронної пошти та спробуйте ще раз, або зв'яжіться з підтримкою для отримання допомоги",
   CredentialsSignin:
-    'Помилка входу: Наданий ідентифікатор або пароль недійсні. Будь ласка, переконайтеся, що ваш ідентифікатор та пароль вірні, або скористайтеся іншим методом входу.',
+    'Наданий ідентифікатор або пароль недійсні. Будь ласка, переконайтеся, що ваш ідентифікатор та пароль вірні, або скористайтеся іншим методом входу',
   SessionRequired:
-    "Потрібна сесія: Для доступу до цього вмісту ви повинні бути авторизовані постійно. Будь ласка, увійдіть, або зв'яжіться з підтримкою, якщо вам потрібна допомога.",
+    "Для доступу до цього вмісту ви повинні бути авторизовані постійно. Будь ласка, увійдіть, або зв'яжіться з підтримкою, якщо вам потрібна допомога",
   Default:
-    "Упс! Щось пішло не так. Будь ласка, спробуйте ще раз, і якщо проблема продовжується, зв'яжіться із нашою службою підтримки.",
+    "Щось пішло не так... Будь ласка, спробуйте ще раз, і якщо проблема продовжується, зв'яжіться із нашою службою підтримки",
 };
-// export const NextAuthErrorDictionary = {
-//   Configuration:
-//     "Oops! We're having trouble with our server configuration. Please double-check your settings to make sure everything's in order.",
-//   AccessDenied:
-//     'Access Denied! You may have been restricted from accessing this feature. If you believe this is a mistake, please contact support.',
-//   Verification:
-//     'Verification Error: The token has either expired or has already been used. Please request a new token or contact support for assistance.',
-//   OAuthSignin:
-//     'Oops! There was an issue in preparing the authorization. Please try again, and if the problem persists, let us know.',
-//   OAuthCallback:
-//     'Error Processing: We encountered a problem while processing the response from the OAuth provider. Please try again, and if the issue continues, contact us.',
-//   OAuthCreateAccount:
-//     "Account Creation Failed: We couldn't create your account with the OAuth provider. Please try a different sign-up method or contact support.",
-//   EmailCreateAccount:
-//     "Account Creation Failed: We couldn't create your account. Please double-check your email and try again, or contact support for assistance.",
-//   Callback:
-//     'Callback Error: Something went wrong with our callback handling. Please try again, and if the issue persists, let us know.',
-//   OAuthAccountNotLinked:
-//     'Account Not Linked: Your email is already associated with another account, but not this one. Please log in with the original account.',
-//   EmailSignin:
-//     "Email Verification Failed: We couldn't send the email with the verification token. Please check your email address and try again or contact support.",
-//   CredentialsSignin:
-//     'Sign-In Failed: The provided credentials are not valid. Please ensure your username and password are correct, or use a different method to sign in.',
-//   SessionRequired:
-//     'Session Required: You must be signed in to access this content. Please log in, or contact support if you need assistance.',
-//   Default:
-//     'Oops! Something went wrong. Please try again, and if the issue persists, contact our support team for assistance.',
-// };
