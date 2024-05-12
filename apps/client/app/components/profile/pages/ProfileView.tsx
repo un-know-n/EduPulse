f11a70dd622c4f6e480e55c535a10ee91e4bf246
Articso <101022731+Articso@users.noreply.github.com>
2024-05-12 22:23:14 +0300

    FEAT(client): Add course search on profile page

--- apps/client/app/components/profile/pages/ProfileView.tsx
+++ apps/client/app/components/profile/pages/ProfileView.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useState, useEffect } from 'react';
 import {
   Box,
   Input,
@@ -12,10 +12,28 @@ import { CoursesList } from '../../course/shared/list/CoursesList';
 import NoCreatedCourses from '../../shared/posters/NoCreatedCourses';
 import { useTypedSelector } from '../../../lib/hooks/redux';
 import { ProfileLayout } from '../layout/ProfileLayout';
+import { useDebounce } from 'apps/client/app/lib/hooks/useDebounce';
 
 export const ProfileView: FC = () => {
   const { data, isLoading, isError } = useGetCreatedCoursesQuery(null);
   const user = useTypedSelector((state) => state.user);
+  const [search, setSearch] = useState('');
+  const [filteredData, setFilteredData] = useState<any[]>([]);
+
+  const debouncedSearch = useDebounce(search, 1500);
+
+  useEffect(() => {
+    if (data) {
+      if (debouncedSearch) {
+        const filtered = data.filter((course) =>
+          course.title.toLowerCase().includes(debouncedSearch.toLowerCase()),
+        );
+        setFilteredData(filtered);
+      } else {
+        setFilteredData(data);
+      }
+    }
+  }, [debouncedSearch, data]);
 
   return (
     <ProfileLayout>
@@ -37,12 +55,16 @@ export const ProfileView: FC = () => {
           <InputLeftElement pointerEvents='none'>
             <FiSearch color='gray' />
           </InputLeftElement>
-          <Input placeholder='Назва курсу' />
+          <Input
+            placeholder='Назва курсу'
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
         </InputGroup>
         <CoursesList
           isLoading={isLoading}
           poster={<NoCreatedCourses />}
-          courses={data}
+          courses={filteredData}
         />
       </Box>
     </ProfileLayout>
